9b19e1186fd60184a73088b9308dc9ef29b58a34
sebamon <sebamon12@gmail.com>
2020-09-11 21:53:27 -0300

    v2

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo Esta es una aplicación para evaluar mis progresos en la materia PWD 2020.Mis nombres es Sebastian Mon y mi Nro de Legajo 114490
-echo Hola mundo
+echo "Esta es una aplicación para evaluar mis progresos en la materia PWD 2020.Mis nombres es Sebastian Mon y mi Nro de Legajo 114490";
+echo "Hola mundo";
 ?>
